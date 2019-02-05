730e3edf7994fae2c9dac4e68614b9d965b06d4a
Benjami Twose <benja.twose.tech@gmail.com>
2019-02-05 21:43:03 +0100

    developing controlwt test rele

---
+++ controlwt/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError:
+    print(RuntimeError)
+
+# Create your models here.
+LED_PIN = 32
+
+
+def turn_on(request):
+    try:
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.output(LED_PIN, 1)
+        return HttpResponse('')
+
+    except RuntimeError:
+        return HttpResponse('')
+
+
+def turn_off(request):
+    try:
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.output(LED_PIN, 0)
+        return HttpResponse('')
+
+    except RuntimeError:
+        return HttpResponse('')
+
