9d8d0ee4471698e76cd293ef88d459025ed73b20
Benjami Twose <benja.twose.tech@gmail.com>
2019-03-06 11:01:51 +0100

    automatic mode power out test

--- controlwt/models.py
+++ controlwt/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from datetime import datetime
+from datetime import time
 from smtconfigs.models import GeneralConfig
 from regtemp.models import Statistics
 from django.http import HttpResponse
