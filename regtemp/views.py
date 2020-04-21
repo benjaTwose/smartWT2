17006860b923a5eace6a62c9e3e7c7cfe028e856
Benjami Twose <benja.twose.tech@gmail.com>
2020-04-21 17:41:41 +0200

    next button html

--- regtemp/views.py
+++ regtemp/views.py
@@ -36,8 +36,6 @@ def view_register_data(request, n_day):
     data = Register.objects.filter(date_reg_day=n_day).order_by('date_reg')
     return render(request, template_name='chart_template_register.html', context={'data': data,'nDay':n_day})
 
-
-
 def view_compute(request, n_day):
     """ nday in isoweek format 1:Monday ..."""
     if request.method == 'GET':
