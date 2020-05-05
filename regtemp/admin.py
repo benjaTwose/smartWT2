d4f72751d5085af0cd13ed4ab32cbeaee9ddb3fd
Benjami Twose <benja.twose.tech@gmail.com>
2020-05-05 19:01:50 +0200

    register. Implementing zones

--- regtemp/admin.py
+++ regtemp/admin.py
@@ -2,7 +2,10 @@
 from django.contrib import admin
 from regtemp.models import Statistics
 from regtemp.models import Register
+from regtemp.models import Zone
+
 
 
 admin.site.register(Statistics)
 admin.site.register(Register)
+admin.site.register(Zone)
